04ce5bfab2e94fb3f0f388b1602ca73ceac0f3f7
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-03 17:28:52 +0700

    lkkjhg

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         label="Your Name"
         placeholder="Enter your name"
       />
-
+      <TextField variant="outlined" placeholder="text" label="you text" />
       <Button
         variant="contained"
         color="primary"
